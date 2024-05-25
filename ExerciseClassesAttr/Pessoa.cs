4dd491e50a9365da3a04813b4d405f51d3fc9b60
Fabricio Dias <fabricio270909@gmail.com>
2024-05-24 22:48:57 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciseClassesAttr/Pessoa.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseClassesAttr
+{
+    internal class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+        public Pessoa getOlder(Pessoa pessoa1, Pessoa pessoa2)
+        {
+            if (pessoa1.idade > pessoa2.idade)
+            {
+                return pessoa1;
+            } else
+            {
+                return pessoa2;
+            }
+        }
+    }
+}
